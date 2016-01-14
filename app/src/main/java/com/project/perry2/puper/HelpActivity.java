df83f32c579038bbc3f2c37ae7c530ef5df02524
AWireman <andrewwireman4@gmail.com>
2016-01-13 22:42:30 -0500

    added payment, help, settings and history activities

---
+++ app/src/main/java/com/project/perry2/puper/HelpActivity.java
@@ -0,0 +1,38 @@
+package com.project.perry2.puper;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        Button account = (Button)findViewById(R.id.buttonAccount);
+        Button payment = (Button)findViewById(R.id.buttonPayment);
+        Button instructions = (Button)findViewById(R.id.buttonInstru);
+        Button locate = (Button)findViewById(R.id.buttonLocate);
+        Button issue = (Button)findViewById(R.id.buttonIssue);
+
+
+    }
+
+}
