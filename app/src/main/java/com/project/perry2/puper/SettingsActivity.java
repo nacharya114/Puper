951e5a15c5d9eca07e681f37357a8230a226e1e6
AWireman <andrewwireman4@gmail.com>
2016-01-13 22:11:36 -0500

    Merge branch 'master' of /Volumes/HDII/Downloads/Puper with conflicts.

---
+++ app/src/main/java/com/project/perry2/puper/SettingsActivity.java
@@ -0,0 +1,35 @@
+package com.project.perry2.puper;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        Button editAccount = (Button)findViewById(R.id.buttonEdit);
+        Button home = (Button)findViewById(R.id.buttonHome);
+        Button work = (Button)findViewById(R.id.buttonWork);
+        Button signOut = (Button)findViewById(R.id.buttonSignOut);
+    }
+
+}
